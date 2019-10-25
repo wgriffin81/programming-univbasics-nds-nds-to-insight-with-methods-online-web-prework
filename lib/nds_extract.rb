15ed3db0292c28008f321c7b9d2dc835c998c47c
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-25 23:08:35 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,7 +18,13 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  pp director_data
+  coorinate_total = 0 
+  inner_len = nds[row_index][colum_index].length 
+  inner_index = 0 
+  
+  while inner_index < inner_len do
+    gross_total += nds[director_data]
+  
   
   
 end
