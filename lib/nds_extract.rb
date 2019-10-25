8e9620e26fcf93ecf0b271d4eed1dd90c2abf560
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-25 23:20:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -18,14 +18,14 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  coorinate_total = 0 
+  gross_total = 0 
   inner_len = nds[row_index][column_index].length 
   inner_index = 0 
   
   while inner_index < inner_len do
     gross_total += nds[director_data][:movies][inner_index][:worldwide_grosses]
     inner_index += 1 
-  
-  
+  end
+  gross_total
   
 end
