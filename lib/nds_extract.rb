6d7aa233a3e640d3fe40bcca4ea412606b65dbae
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-25 23:58:42 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -12,10 +12,10 @@ def directors_totals(nds)
  while director_index < nds.size do
    director = nds[director_index]
    result[director[:name]] = gross_for_director(director)
+   director_index += 1 
+ end
+ result
  
- 
-  
-  
 end
 
 
