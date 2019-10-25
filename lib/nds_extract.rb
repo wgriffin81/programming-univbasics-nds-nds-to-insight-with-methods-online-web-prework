bc543ea2337ec22e884c1c95ccaaecd850f748a6
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-25 23:59:15 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -28,7 +28,7 @@ def gross_for_director(director_data)
   gross = 0 
   
   while index < director_data[:movies].length do
-    gross += director_data[:movies][index][:worldwide_grosses]
+    gross += director_data[:movies][index][:worldwide_gross]
     index += 1 
   end
   gross
